0e20c4b1c80838cc3ed3c809a6e35e7e9c84883b
Wahyu <whyuuuuau@gmail.com>
2022-10-12 23:58:05 +0700

    frontend

--- src/java/config/HibernateUtil.java
+++ src/java/config/HibernateUtil.java
@@ -8,9 +8,9 @@ package config;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
-import org.hibernate.service.ServiceRegistryBuilder;
 
 /**
  *
@@ -22,7 +22,7 @@ public class HibernateUtil {
     
     public Session openSession(Class annotatedClass) {
         cfg = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(annotatedClass);
-        ServiceRegistry reg = new ServiceRegistryBuilder().applySettings(cfg.getProperties()).build();
+        ServiceRegistry reg = new StandardServiceRegistryBuilder().applySettings(cfg.getProperties()).build();
         
         SessionFactory sf = cfg.buildSessionFactory(reg);
         Session session = sf.openSession();
