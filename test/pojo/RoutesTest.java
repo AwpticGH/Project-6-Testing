3914adc1d5a86cfb88936da1132647eb18415abf
CCIT OCR1 1-7 <CCIT.OCR1.17@gmail.com>
2022-11-03 16:10:13 +0700

    Progress

--- test/pojo/RoutesTest.java
+++ test/pojo/RoutesTest.java
@@ -5,6 +5,8 @@
  */
 package pojo;
 
+import dao.AirportsDao;
+import dao.RoutesDao;
 import java.util.List;
 import java.util.Set;
 import org.junit.After;
@@ -20,6 +22,20 @@ import static org.junit.Assert.*;
  */
 public class RoutesTest {
     
+    RoutesDao dao = new RoutesDao();
+    Routes createRoute;
+    Routes updateTrue;
+    Routes updateFalse;
+    Routes deleteTrue;
+    Routes deleteFalse;
+    
+    AirportsDao airportDao = new AirportsDao();
+    Airports departure = airportDao.getById(23);
+    Airports destination = airportDao.getById(33);
+    
+    int createId = 8111;
+    int timeOfFlight = 120;
+    
     public RoutesTest() {
     }
     
@@ -33,199 +49,24 @@ public class RoutesTest {
     
     @Before
     public void setUp() {
+        createRoute = new Routes(createId, departure, destination, timeOfFlight);
+        
+        updateTrue = dao.getById(8112);
+        updateTrue.setAirportsByDepartureId(departure);
+        updateTrue.setAirportsByDestinationId(destination);
+        updateTrue.setTimeOfFlight(timeOfFlight);
+        
+        updateFalse = dao.getById(8113);
+        updateFalse.setId(null);
+        
+        deleteTrue = dao.getById(8114);
+        
+        deleteFalse = dao.getById(8115);
+        deleteFalse.setId(null);
     }
     
     @After
     public void tearDown() {
-    }
-
-    /**
-     * Test of getFrom method, of class Routes.
-     */
-    @Test
-    public void testGetFrom() {
-        System.out.println("getFrom");
-        Routes instance = new Routes();
-        int expResult = 0;
-        int result = instance.getFrom();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of setFrom method, of class Routes.
-     */
-    @Test
-    public void testSetFrom() {
-        System.out.println("setFrom");
-        int from = 0;
-        Routes instance = new Routes();
-        instance.setFrom(from);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getTo method, of class Routes.
-     */
-    @Test
-    public void testGetTo() {
-        System.out.println("getTo");
-        Routes instance = new Routes();
-        int expResult = 0;
-        int result = instance.getTo();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of setTo method, of class Routes.
-     */
-    @Test
-    public void testSetTo() {
-        System.out.println("setTo");
-        int to = 0;
-        Routes instance = new Routes();
-        instance.setTo(to);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getId method, of class Routes.
-     */
-    @Test
-    public void testGetId() {
-        System.out.println("getId");
-        Routes instance = new Routes();
-        Integer expResult = null;
-        Integer result = instance.getId();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of setId method, of class Routes.
-     */
-    @Test
-    public void testSetId() {
-        System.out.println("setId");
-        Integer id = null;
-        Routes instance = new Routes();
-        instance.setId(id);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getAirportsByDestinationId method, of class Routes.
-     */
-    @Test
-    public void testGetAirportsByDestinationId() {
-        System.out.println("getAirportsByDestinationId");
-        Routes instance = new Routes();
-        Airports expResult = null;
-        Airports result = instance.getAirportsByDestinationId();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of setAirportsByDestinationId method, of class Routes.
-     */
-    @Test
-    public void testSetAirportsByDestinationId() {
-        System.out.println("setAirportsByDestinationId");
-        Airports airportsByDestinationId = null;
-        Routes instance = new Routes();
-        instance.setAirportsByDestinationId(airportsByDestinationId);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getAirportsByDepartureId method, of class Routes.
-     */
-    @Test
-    public void testGetAirportsByDepartureId() {
-        System.out.println("getAirportsByDepartureId");
-        Routes instance = new Routes();
-        Airports expResult = null;
-        Airports result = instance.getAirportsByDepartureId();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of setAirportsByDepartureId method, of class Routes.
-     */
-    @Test
-    public void testSetAirportsByDepartureId() {
-        System.out.println("setAirportsByDepartureId");
-        Airports airportsByDepartureId = null;
-        Routes instance = new Routes();
-        instance.setAirportsByDepartureId(airportsByDepartureId);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getTimeOfFlight method, of class Routes.
-     */
-    @Test
-    public void testGetTimeOfFlight() {
-        System.out.println("getTimeOfFlight");
-        Routes instance = new Routes();
-        int expResult = 0;
-        int result = instance.getTimeOfFlight();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of setTimeOfFlight method, of class Routes.
-     */
-    @Test
-    public void testSetTimeOfFlight() {
-        System.out.println("setTimeOfFlight");
-        int timeOfFlight = 0;
-        Routes instance = new Routes();
-        instance.setTimeOfFlight(timeOfFlight);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getFlightses method, of class Routes.
-     */
-    @Test
-    public void testGetFlightses() {
-        System.out.println("getFlightses");
-        Routes instance = new Routes();
-        Set expResult = null;
-        Set result = instance.getFlightses();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of setFlightses method, of class Routes.
-     */
-    @Test
-    public void testSetFlightses() {
-        System.out.println("setFlightses");
-        Set flightses = null;
-        Routes instance = new Routes();
-        instance.setFlightses(flightses);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
 
     /**
@@ -235,11 +76,8 @@ public class RoutesTest {
     public void testGetAll() {
         System.out.println("getAll");
         Routes instance = new Routes();
-        List<Routes> expResult = null;
         List<Routes> result = instance.getAll();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        assertNotNull(result);
     }
 
     /**
@@ -249,53 +87,77 @@ public class RoutesTest {
     public void testGetById() {
         System.out.println("getById");
         Routes instance = new Routes();
-        String expResult = "";
+        instance.setId(7120);
+        String expResult = "./edit/edit_routes.xhtml";
         String result = instance.getById();
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
 
     /**
      * Test of create method, of class Routes.
      */
     @Test
-    public void testCreate() {
-        System.out.println("create");
+    public void testCreateTrue() {
+        System.out.println("createTrue");
+        String expResult = "../routes.xhtml";
+        String result = createRoute.create();
+        assertEquals(expResult, result);
+    }
+    
+    /**
+     * Test of create method, of class Routes.
+     */
+    @Test
+    public void testCreateFalse() {
+        System.out.println("createFalse");
         Routes instance = new Routes();
-        String expResult = "";
+        String expResult = "./create/create_routes.xhtml";
         String result = instance.create();
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
 
     /**
      * Test of update method, of class Routes.
      */
     @Test
-    public void testUpdate() {
-        System.out.println("update");
-        Routes instance = new Routes();
-        String expResult = "";
-        String result = instance.update();
+    public void testUpdateTrue() {
+        System.out.println("updateTrue");
+        String expResult = "../routes.xhtml";
+        String result = updateTrue.update();
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    }
+    
+    /**
+     * Test of update method, of class Routes.
+     */
+    @Test
+    public void testUpdateFalse() {
+        System.out.println("updateFalse");
+        String expResult = "./edit/edit_routes.xhtml";
+        String result = updateFalse.update();
+        assertEquals(expResult, result);
     }
 
     /**
      * Test of delete method, of class Routes.
      */
     @Test
-    public void testDelete() {
-        System.out.println("delete");
-        Routes instance = new Routes();
-        String expResult = "";
-        String result = instance.delete();
+    public void testDeleteTrue() {
+        System.out.println("deleteTrue");
+        String expResult = "../routes.xhtml";
+        String result = deleteTrue.delete();
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    }
+    
+    /**
+     * Test of delete method, of class Routes.
+     */
+    @Test
+    public void testDeleteFalse() {
+        System.out.println("deleteFalse");
+        String expResult = "./edit/edit_routes.xhtml";
+        String result = deleteFalse.delete();
+        assertEquals(expResult, result);
     }
     
 }
